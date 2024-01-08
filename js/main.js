c5c004fc63878540f3bed579bae855346c1fe045
Juhyun Min <xxo3082@naver.com>
2024-01-09 00:41:38 +0900

    added badges

--- js/main.js
+++ js/main.js
@@ -15,3 +15,20 @@ searchInputEl.addEventListener('blur', function () {
   searchInputEl.setAttribute('placeholder', '')
 })
 
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300))
+
